0ec20d2140a34fff16dd9018e15dc62a7db6ec1d
Tshegofatso <tmolobe15@gmail.com>
2019-03-14 17:06:50 +0200

    Commit: created a board, player, changed startingposition, still busy editing getseeds

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -2,18 +2,49 @@
 
 type StartingPosition =
     | South
-    | North
+    | North 
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board = 
+    let rec get seeds =
+        let housenum = (1, 2, 3, 4, 5, 6)
+        let a, b, c, d, e, f = housenum
+        match housenum with 
+        |seeds -> housenum
+        |_ -> failwith "Not Implemented"
+
+failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+//game should start at the southern end 
+let start position = 
+    match position with 
+    |South -> StartingPosition.South
+    |_ -> failwith "No Player available"
 
-let score board = failwith "Not implemented"
+let score board = 
+    failwith "Not implemented"
 
-let gameState board = failwith "Not implemented"
+let gameState board = 
+     failwith "Not implemented"
 
+
+//Player: a player owns houses and seeds
+type Player = 
+    {   houses: int * int * int * int * int *  int
+        seeds: int                                       //48 in total
+    }
+
+//Board: The board contains players, scoreboards and the general state of the game
+type Board = 
+    {  
+        scoreboards: int * int  
+        state: string                              
+        players1: Player           
+        players2: Player 
+    }
+
+ 
 [<EntryPoint>]
 let main _ =
     printfn "Hello from F#!"
